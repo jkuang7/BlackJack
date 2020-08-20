c8caba3378fc28fad9c7647f7ef057154f1f5e39
jkuang7 <52064138+jkuang7@users.noreply.github.com>
2020-08-20 03:40:01 -0400

    Update database_handler.php

--- includes/database_handler.php
+++ includes/database_handler.php
@@ -1,10 +1,10 @@
 <?php
 
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "pokerface";
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+// $dbServername = "localhost";
+// $dbUsername = "root";
+// $dbPassword = "";
+// $dbName = "pokerface";
+// $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 // $dbServername = "mars.cs.qc.cuny.edu: 3306";
 // $dbUsername = "kuji0700";
@@ -12,11 +12,11 @@ $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 // $dbName = "kuji0700";
 // $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-// $dbServername = "us-cdbr-east-02.cleardb.com";
-// $dbUsername = "b172fae6a51ed5";
-// $dbPassword = "63c0ad12";
-// $dbName = "heroku_8f1c5941b4063e2";
-// $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+$dbServername = "us-cdbr-east-02.cleardb.com";
+$dbUsername = "b172fae6a51ed5";
+$dbPassword = "63c0ad12";
+$dbName = "heroku_8f1c5941b4063e2";
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
 if(!$conn) {
     die("Connection failed: ".mysqli_connect_error());
