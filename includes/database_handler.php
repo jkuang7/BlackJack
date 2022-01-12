92ba7925fdbc41e11ba1b9e53c07eb2c947a3461
Jian Kuang <52064138+jkuang7@users.noreply.github.com>
2022-01-12 17:55:51 -0500

    Update database_handler.php

--- includes/database_handler.php
+++ includes/database_handler.php
@@ -6,12 +6,6 @@
 // $dbName = "pokerface";
 // $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
-// $dbServername = "mars.cs.qc.cuny.edu: 3306";
-// $dbUsername = "kuji0700";
-// $dbPassword = "23370700";
-// $dbName = "kuji0700";
-// $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
 $dbServername = "us-cdbr-east-02.cleardb.com";
 $dbUsername = "b172fae6a51ed5";
 $dbPassword = "63c0ad12";
